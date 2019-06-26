d36d5cbffe4fb21d224494f46859ee48919f19d8
Unknown <vanessa.osterberg@outlook.com>
2019-06-25 20:46:43 -0500

    Barrel Model Created

---
+++ EntityFrameworkExample/Data/Configurations/BarrelConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using EntityFrameworkExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkExample.Data.Configurations
+{
+    public class BarrelConfiguration:EntityTypeConfiguration<Barrel>
+    {
+        public BarrelConfiguration()
+        {
+            HasKey(b => b.Id);
+        }
+    }
+}
---
+++ EntityFrameworkExample/Migrations/201906260144327_Barrel.cs
@@ -0,0 +1,57 @@
+namespace EntityFrameworkExample.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Barrel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Barrels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Radius = c.Double(nullable: false),
+                        Height = c.Double(nullable: false),
+                        ConstructionMaterial = c.String(),
+                        Contents = c.String(),
+                        CurrentLocation = c.String(),
+                        DateCreated = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ExampleEntities",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ExampleStringData = c.String(),
+                        ExampleIntegerData = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ExampleChildEntities",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ExampleData = c.String(),
+                        ParentEntityId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ExampleEntities", t => t.ParentEntityId, cascadeDelete: true)
+                .Index(t => t.ParentEntityId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ExampleChildEntities", "ParentEntityId", "dbo.ExampleEntities");
+            DropIndex("dbo.ExampleChildEntities", new[] { "ParentEntityId" });
+            DropTable("dbo.ExampleChildEntities");
+            DropTable("dbo.ExampleEntities");
+            DropTable("dbo.Barrels");
+        }
+    }
+}
