c0bff9637e2aecf07bb5b8f6471df622675d3dc4
Unknown <vanessa.osterberg@outlook.com>
2019-06-25 21:14:46 -0500

    Added Index and Create

---
+++ EntityFrameworkExample/Controllers/BarrelController.cs
@@ -0,0 +1,43 @@
+﻿using EntityFrameworkExample.Models;
+using EntityFrameworkExample.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkExample.Controllers
+{
+    public class BarrelController : Controller
+    {
+        public BarrelService service = new BarrelService();
+        // GET: Barrel
+        public ActionResult Index()
+        {
+            return View(service.GetAllBarrels());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Barrel toAdd)
+        {
+            if (ModelState.IsValid)
+            {
+                toAdd.DateCreated = DateTime.Now;
+                service.AddBarrel(toAdd);
+                return RedirectToAction("Index");
+            }
+            return View(toAdd);
+            
+        }
+
+
+
+
+    }
+}
---
+++ EntityFrameworkExample/Repositories/BarrelRepository.cs
@@ -0,0 +1,30 @@
+﻿using EntityFrameworkExample.Data.Context;
+using EntityFrameworkExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkExample.Repositories
+{
+    public class BarrelRepository
+    {
+        public DataContext dbContext;
+
+        public BarrelRepository()
+        {
+            dbContext = new DataContext();
+        }
+
+        public List<Barrel> GetAllBarrels()
+        {
+            return dbContext.Barrels.ToList();
+        }
+
+        public void AddBarrel(Barrel toAdd)
+        {
+            dbContext.Barrels.Add(toAdd);
+            dbContext.SaveChanges();
+        }
+    }
+}
