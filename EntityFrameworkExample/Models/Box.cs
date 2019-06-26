14e4b3ff025a52dcb8715090908c2e8216314b93
Unknown <vanessa.osterberg@outlook.com>
2019-06-26 08:17:50 -0500

    Finished Creating Model

--- EntityFrameworkExample/Models/Box.cs
+++ EntityFrameworkExample/Models/Box.cs
@@ -10,7 +10,7 @@ namespace EntityFrameworkExample.Models
     {
         [Key]
         public int Id { get; set; }
-        public double width { get; set; }
-        public double height { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
     }
 }
