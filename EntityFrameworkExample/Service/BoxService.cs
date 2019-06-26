2baa99c620694453a55c85d64f5ef4eae99971d7
Unknown <vanessa.osterberg@outlook.com>
2019-06-26 08:31:53 -0500

    Index and Create Work

---
+++ EntityFrameworkExample/Service/BoxService.cs
@@ -0,0 +1,29 @@
+﻿using EntityFrameworkExample.Models;
+using EntityFrameworkExample.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkExample.Service
+{
+    public class BoxService
+    {
+        public BoxRepository repository;
+
+        public BoxService()
+        {
+            repository = new BoxRepository();
+        }
+
+        public List<Box> GetAllBoxes()
+        {
+            return repository.GetAllBoxes();
+        }
+
+        public void AddBox(Box toAdd)
+        {
+            repository.AddBox(toAdd);
+        }
+    }
+}
