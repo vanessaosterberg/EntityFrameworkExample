4a86180a059db88718a8d0b4a45dc0b1ca21567a
Unknown <vanessa.osterberg@outlook.com>
2019-06-26 07:51:30 -0500

    Created Model

---
+++ EntityFrameworkExample/Models/Box.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkExample.Models
+{
+    public class Box
+    {
+        [Key]
+        public int Id { get; set; }
+        public double width { get; set; }
+        public double height { get; set; }
+    }
+}
